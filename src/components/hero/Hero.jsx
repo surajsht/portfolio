705a593c643240f91cfcf63dac46bb6b36fc54c4
suraj <surajsht2055@gmail.com>
2024-06-09 12:45:45 +0545

    hero section done

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,20 @@
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <section id="home" className="hero">
+      <div className="wrapper">
+        <div className="hero-content">
+          <h2 className="hero-title">
+            <span>Hello, I'm</span> Suraj Kumar Shrestha, a passionate Frontend
+            Developer
+          </h2>
+
+          <h3 className="hero-sub-title"></h3>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
